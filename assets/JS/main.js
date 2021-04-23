d30644fc2c29ad74e6846a2b9adad48690693508
Mirko Chiavaroli <mirkochiavaroli@gmail.com>
2021-04-23 19:03:37 +0200

    first commit

---
+++ assets/JS/main.js
@@ -0,0 +1,26 @@
+const $ = document;
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page chargée"); //andiamo a vedere se la pagina si è caricata nella console log nel sito cmd+opt+i
+
+  //   $.addEventListener("click", () => {
+  //     console.log("CLICCHETE !! "); //verifica i click sulla pagina
+  //   });
+  $.querySelector("#contact-form").addEventListener("submit", async (event) => {
+    // la fonction preventDefault() annula il refresh della pagina
+    event.preventDefault();
+
+    const data = {
+      firstName: $.querySelector("#firstname").value,
+      lastName: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      message: $.querySelector("#message").value,
+    };
+
+    console.log(data);
+
+    const response = await axios.post("http://localhost:3000/form", data);
+    console.log(response);
+    alert("Merci pour le form");
+  });
+});
